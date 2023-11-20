39f87bb8c41ff4347688d168a83a54b3385bb342
Russ Painter <russp@geekymonkey.com>
2023-11-20 19:05:23 +0000

    Asteroid model with random rotate

---
+++ Objects/Asteroids/Asteroid.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+
+@export var SpinSpeed: float = 100.0
+@export var MoveSpeed: float = 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	RandomRotation()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func RandomRotation() -> void:
+	var rand = RandomNumberGenerator.new()
+	var randAngle = Vector3(rand.randf(), rand.randf(), rand.randf()).normalized()
+	rotate_object_local(randAngle, rand.randf_range(0, PI*2))
+	angular_velocity = randAngle * (rand.randf() * 2 - 1) * SpinSpeed
+	linear_velocity = randAngle * (rand.randf() * 2 - 1) * MoveSpeed
