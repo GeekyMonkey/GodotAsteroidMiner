43cd0c4e96065d9401a7ceaeb1f4a5e926d77b5f
Russ Painter <russp@geekymonkey.com>
2023-11-28 19:07:41 +0000

    Bouncing Lasers

--- Objects/FX/Laser.gd
+++ Objects/FX/Laser.gd
@@ -8,6 +8,8 @@ const Sparks = preload("res://Objects/FX/Sparks.tscn")
 
 var Age: float = 0
 var init: bool = false
+var ColMask: int
+var ColLayer: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,6 +24,8 @@ func _physics_process(delta: float) -> void:
 func Init():
 	if !init:
 		init = true
+		ColMask = collision_mask
+		ColLayer = collision_layer
 		var forward_dir = transform.basis.y  # Get the forward direction
 		set_linear_velocity(forward_dir * Speed)  # Set the velocity
 
@@ -37,12 +41,28 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 
 		var other = state.get_contact_collider_object(0)
 		print("Laser collision at: " + str(collisionPos) + " @ " + str(force))
-		if other is Asteroid:
+		if other is Asteroid or true:
 			Events.LaserHitAsteroid.emit(self, other, collisionPos, force)
+
+			var norm = state.get_contact_local_normal(0)
+			angular_velocity = Vector3.ZERO
+			var forward_dir: Vector3 = transform.basis.y.bounce(norm).normalized()
+			set_linear_velocity(forward_dir * Speed)
+			look_at_from_position(global_position, global_position + forward_dir, Vector3.FORWARD, true)
+			rotate_object_local(Vector3.MODEL_RIGHT, 90)
+			collision_mask = 0
+			collision_layer = 0
+
+			var spark = Sparks.instantiate()
+			spark.position = collisionPos
+			get_parent().add_child(spark)
+
+			await DelayExtensions.Seconds(0.08)
+			collision_mask = ColMask
+			collision_layer = ColLayer
+
+
 		else:
 			print("Laser hit: " + other.name)
 
-		var spark = Sparks.instantiate()
-		spark.position = collisionPos
-		get_parent().add_child(spark)
-		queue_free()
+		#queue_free()
